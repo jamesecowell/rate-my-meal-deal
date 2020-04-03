4e65111082be878aa5495b21f572d214a7d613a9
James Cowell <james.cowell28@gmail.com>
2020-04-03 14:56:04 +0100

    added App class and header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Rate your meal deal!</h1>
+      <h2>Choose a main, snack and drink</h2>
+      <p>
+        Your decisions will be subject to intense judgement - choose wisely!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
